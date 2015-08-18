0228742ff446ce38c04c16a55e4d5a77a0841474
Kimberly Lowe-Williams <kimberlylowewilliams@gmail.com>
2015-08-18 08:10:43 -0500

    fixed issue with carrierwave file connecting to AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:               'AWS',
-    aws_access_key_id:      'S3_KEY',
-    aws_secret_access_key:  'S3_SECRET',
+    aws_access_key_id:      ENV['S3_KEY'],
+    aws_secret_access_key:  ENV['S3_SECRET']
   }
 
   # For testing, upload files to local `tmp` folder.
@@ -15,5 +15,5 @@ CarrierWave.configure do |config|
   end
 
   config.cache_dir        = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on Heroku
-  config.fog_directory    = 'S3_BUCKET_NAME'
+  config.fog_directory    = ENV['S3_BUCKET_NAME']
 end
